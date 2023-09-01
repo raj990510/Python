f5a467532ac8d998bd3e91970c4bb1b499c057ae
raj990510 <143532132+raj990510@users.noreply.github.com>
2023-09-01 17:31:18 +0530

    Add files via upload

---
+++ We_find_divide.py
@@ -0,0 +1,7 @@
+Num1=int(input("Enter a  number: ")) # Given a first input
+Num2=int(input("Enter a  number: ")) # Given a second input
+
+if  Num2 == 0: #We check denominator is not Zero
+  print("please given a vaild number")
+else :
+  print(Num1/Num2) #We divide first input and second input
